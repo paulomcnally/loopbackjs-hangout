a1549afd8756bafb0110ba7b3311ce3c9d230359
Paulo McNally <paulomcnally@gmail.com>
2014-10-02 20:12:02 -0600

    add validation

--- api/common/models/sms.js
+++ api/common/models/sms.js
@@ -3,7 +3,17 @@ module.exports = function(Sms) {
   Sms.beforeRemote('create',
     function(ctx, sms, next) {
       ctx.req.body['create_dt'] = new Date();
-      next();
+
+      if (ctx.req.body.phone.length != 8) {
+
+        var err = new Error('El número celular debe tener 8 dígitos númericos');
+        err.statusCode = 409;
+        next(err);
+      }
+      else {
+        next();
+      }
+
     }
   )
 
